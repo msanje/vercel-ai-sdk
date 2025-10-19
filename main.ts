2abec8abcfee3419a5b72ff5853af8d58608a277
msanje <m.sanjayachar@gmail.com>
2025-10-19 08:50:59 +0530

    03 System prompts

--- main.ts
+++ main.ts
@@ -1,23 +1,40 @@
 import { anthropic } from "@ai-sdk/anthropic";
-import { generateText, streamText } from "ai";
+import { generateText } from "ai";
 
 const model = anthropic("claude-3-5-haiku-latest");
 
 export const answerMyQuestion = async (prompt: string) => {
-  const { textStream, text } = streamText({
+  const { text } = await generateText({
     model,
-    prompt,
+    // this prepends a message with the role of system under the hood
+    // system:
+    //   `You are a text summarizer. ` +
+    //   `Summarize the text you receive. ` +
+    //   `Be concise. ` +
+    //   `Return only the summary. ` +
+    //   `Do not use the phrase "here is a summary". ` +
+    //   `Highlight relevant phrases in bold. ` +
+    //   `The summary should be two sentences long. `,
+
+    // this can be done in generateText streamText and all of the other api's
+    messages: [
+      {
+        role: "system",
+        content:
+          `You are a text summarizer. ` +
+          `Summarize the text you receive. ` +
+          `Be concise. ` +
+          `Return only the summary. ` +
+          `Do not use the phrase "here is a summary". ` +
+          `Highlight relevant phrases in bold. ` +
+          `The summary should be two sentences long. `,
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
   });
 
-  // complete answer
-  const finalText = await text;
-
-  // streaming to stdout - terminal/console
-  for await (const text of textStream) {
-    process.stdout.write(text);
-  }
-
-  return textStream;
+  return text;
 };
-
-await answerMyQuestion("What is the color of the sun ?");
