dc40fc2a252d0669d15d49c1876454de5bb3a6ac
msanje <m.sanjayachar@gmail.com>
2025-10-19 09:19:11 +0530

    04 Hot-swapping models

--- main.ts
+++ main.ts
@@ -1,40 +1,18 @@
+import { generateText, type LanguageModel } from "ai";
 import { anthropic } from "@ai-sdk/anthropic";
-import { generateText } from "ai";
+import { openai } from "@ai-sdk/openai";
 
-const model = anthropic("claude-3-5-haiku-latest");
-
-export const answerMyQuestion = async (prompt: string) => {
+export const ask = async (prompt: string, model: LanguageModel) => {
   const { text } = await generateText({
     model,
-    // this prepends a message with the role of system under the hood
-    // system:
-    //   `You are a text summarizer. ` +
-    //   `Summarize the text you receive. ` +
-    //   `Be concise. ` +
-    //   `Return only the summary. ` +
-    //   `Do not use the phrase "here is a summary". ` +
-    //   `Highlight relevant phrases in bold. ` +
-    //   `The summary should be two sentences long. `,
-
-    // this can be done in generateText streamText and all of the other api's
-    messages: [
-      {
-        role: "system",
-        content:
-          `You are a text summarizer. ` +
-          `Summarize the text you receive. ` +
-          `Be concise. ` +
-          `Return only the summary. ` +
-          `Do not use the phrase "here is a summary". ` +
-          `Highlight relevant phrases in bold. ` +
-          `The summary should be two sentences long. `,
-      },
-      {
-        role: "user",
-        content: prompt,
-      },
-    ],
+    prompt,
   });
 
   return text;
 };
+
+const prompt = `Tell me a story about your grandmother.`;
+
+const anthropicResult = await ask(prompt, anthropic("claude-3-5-haiku-latest"));
+
+const openaiResult = await ask(prompt, openai("gpt-4o-mini-2024-07-18"));
