67b3a6c47d6638149977efa3553805ee1b01966e
msanje <m.sanjayachar@gmail.com>
2025-10-19 08:40:11 +0530

    02 Streaming Text

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
 import { anthropic } from "@ai-sdk/anthropic";
-import { generateText } from "ai";
+import { generateText, streamText } from "ai";
 
 const model = anthropic("claude-3-5-haiku-latest");
 
 export const answerMyQuestion = async (prompt: string) => {
-  const { text } = await generateText({
+  const { textStream, text } = streamText({
     model,
     prompt,
   });
 
-  return text;
+  // complete answer
+  const finalText = await text;
+
+  // streaming to stdout - terminal/console
+  for await (const text of textStream) {
+    process.stdout.write(text);
+  }
+
+  return textStream;
 };
 
-const answer = await answerMyQuestion(
-  "What is the chemical formula for dihydrogen monoxide",
-);
-
-console.log(answer);
+await answerMyQuestion("What is the color of the sun ?");
