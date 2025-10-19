8b3a2e283d0252a3a546889294d88f0571223935
msanje <m.sanjayachar@gmail.com>
2025-10-19 08:35:32 +0530

    01 Generating text

---
+++ README.md
@@ -0,0 +1,15 @@
+# vercel-ai-sdk
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run 
+```
+
+This project was created using `bun init` in bun v1.2.12. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ main.ts
@@ -0,0 +1,19 @@
+import { anthropic } from "@ai-sdk/anthropic";
+import { generateText } from "ai";
+
+const model = anthropic("claude-3-5-haiku-latest");
+
+export const answerMyQuestion = async (prompt: string) => {
+  const { text } = await generateText({
+    model,
+    prompt,
+  });
+
+  return text;
+};
+
+const answer = await answerMyQuestion(
+  "What is the chemical formula for dihydrogen monoxide",
+);
+
+console.log(answer);
